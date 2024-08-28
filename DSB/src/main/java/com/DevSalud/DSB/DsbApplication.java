7331ac2f999a6e7a4b15fa5717d7e5c43b8fd966
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-08-27 22:45:52 -0500

    DSB-ProAula😎

---
+++ DSB/src/main/java/com/DevSalud/DSB/DsbApplication.java
@@ -0,0 +1,13 @@
+package com.DevSalud.DSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsbApplication.class, args);
+	}
+
+}
