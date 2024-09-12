38f4f795c45aa952715575356926ef870547bf92
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-09-11 21:13:48 -0500

    DSB-ProAula😎

--- DSB/src/main/java/com/Developper/DSB/Controller/ErrorController.java
+++ DSB/src/main/java/com/Developper/DSB/Controller/ErrorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-    @GetMapping("/Error")
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         String ErrorPage = "Error";
