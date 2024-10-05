7f6c607c58e6153ed4c1ce693143aa146128afb0
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-10-04 23:57:58 -0500

    Modifications of the model
    Co-Authored-By: YublianVerbel <148595186+YublianVerbel@users.noreply.github.com>
    Co-Authored-By: Víctor José Castillo Castro <128097417+victorcast2@users.noreply.github.com>

---
+++ DSB/src/main/java/com/DevSalud/DSB/Model/StatusHealthModel.java
@@ -0,0 +1,24 @@
+package com.DevSalud.DSB.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "StatusHealth")
+public class StatusHealthModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long Id;
+
+    @Basic
+    @NonNull
+    public String HealthClassification;;
+    public float BodyMass;
+
+    @ManyToOne
+    @JoinColumn(name = "UsersId", nullable = false)
+    private UserModel User;
+}
