ec9d41aa8d149cf3b27332ac85c7d403c8835b1c
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-10-09 18:47:44 -0500

    Update

--- DSB/src/main/java/com/DevSalud/DSB/controller/PaginaPrincipalController.java
+++ DSB/src/main/java/com/DevSalud/DSB/controller/PaginaPrincipalController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PaginaPrincipalController {
 
-    @GetMapping({ "DSB", " " })
+    @GetMapping({ "DSB", "/" })
     public String paginaPrincipal() {
-        return "indexPaginaPrincipal";
+        return "IndexPaginaPrincipal";
     }
 
 }
