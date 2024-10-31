45ba1dbbc6397c835c9327dc969d21f6fdd37b35
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-10-31 17:52:14 -0500

    Update - Error Page

--- DSB/src/main/java/com/DevSalud/DSB/Controller/UserController.java
+++ DSB/src/main/java/com/DevSalud/DSB/Controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.DevSalud.DSB.Exception.NoDataFoundException;
 import com.DevSalud.DSB.Model.UserModel;
 import com.DevSalud.DSB.Service.UserServices;
