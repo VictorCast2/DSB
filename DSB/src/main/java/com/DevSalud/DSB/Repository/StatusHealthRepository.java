b3b67da3b201cedbb455361752d776cee7a3a7d6
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-05 19:09:15 -0500

    Delete StatusHealthRepository
    Co-Authored-By: YUBLIAN VERBEL  <148595186+YublianVerbel@users.noreply.github.com>

--- DSB/src/main/java/com/DevSalud/DSB/Repository/StatusHealthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.DevSalud.DSB.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusHealthRepository extends JpaRepository<StatusHealthModel, Long> {
-}
