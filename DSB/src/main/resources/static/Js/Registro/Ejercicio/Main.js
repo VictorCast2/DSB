bf3f98931a947e561f07f5bb8ba3785ab437cca9
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-05 23:37:36 -0500

    UPdate
    Co-Authored-By: keyner reyes <reyeskeyner110@gmail.com>

--- DSB/src/main/resources/static/Js/Registro/Ejercicio/Main.js
+++
@@ -1,4 +0,0 @@
-function onTipoEjercicioChange() {
-            var selectedTipo = document.getElementById('tipoEjercicioSelect').value;
-            window.location.href = '/Api/Users/Exercises/RegistroEjercicio?selectedTipo=' + selectedTipo;
-        }
