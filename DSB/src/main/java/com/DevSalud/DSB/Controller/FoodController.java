5dfeee3892f4779abe96a2f1dea1c11fd234119d
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-06 23:00:11 -0500

    Git Update

--- DSB/src/main/java/com/DevSalud/DSB/Controller/FoodController.java
+++ DSB/src/main/java/com/DevSalud/DSB/Controller/FoodController.java
@@ -1,12 +1,42 @@
 package com.DevSalud.DSB.Controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/Api/Users/Food")
 public class FoodController {
+
+    @ModelAttribute("allCategoriaDesayuno")
+    public List<String> categoriaDelDesayuno() {
+        return Arrays.asList(
+            "Proteinas", "Frutas", "Verduras", "Granos", 
+            "Productos Lacteos", "Azucares", "Grasas Saturadas", 
+            "Harinas", "Carbohidratos"
+        );
+    }
+
+    @ModelAttribute("allCategoriaAlmuerzo")
+    public List<String> categoriaDelAlmuerzo() {
+        return Arrays.asList(
+            "Proteinas", "Verduras", "Granos", "Productos Lacteos", 
+            "Azucares", "Grasas Saturadas", "Harinas", "Carbohidratos"
+        );
+    }
+
+    @ModelAttribute("allCategoriaCena")
+    public List<String> categoriaDeLaCena() {
+        return Arrays.asList(
+            "Proteinas", "Frutas", "Verduras", "Granos", 
+            "Productos Lacteos", "Azucares", "Grasas Saturadas", 
+            "Harinas", "Carbohidratos"
+        );
+    }
 
     @GetMapping("/RegistroAlimento")
     public String formularioRegistroAlimento() {
