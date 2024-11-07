21a959b5762055e7d9607d3e4068f2ea61155bda
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-07 17:46:59 -0500

    Update Java de ejercicio y alimento
    Co-Authored-By: YUBLIAN VERBEL  <148595186+YublianVerbel@users.noreply.github.com>

--- DSB/src/main/java/com/DevSalud/DSB/Controller/FoodController.java
+++ DSB/src/main/java/com/DevSalud/DSB/Controller/FoodController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @Controller
 @RequestMapping(path = "/Api/Users/Food")
 public class FoodController {
