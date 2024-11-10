a4d34007e6dffe07cda1b0330718eb394ecb398c
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-10 15:58:23 -0500

    Update All

--- DSB/src/main/java/com/DevSalud/DSB/Controller/PlanExerciseController.java
+++ DSB/src/main/java/com/DevSalud/DSB/Controller/PlanExerciseController.java
@@ -1,17 +1,10 @@
 package com.DevSalud.DSB.Controller;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 import com.DevSalud.DSB.Model.ExerciseLogModel;
 import com.DevSalud.DSB.Service.PlanExerciseService;
 
@@ -26,7 +19,7 @@ public class PlanExerciseController {
     public Map<String, List<String>> PlanesMap() {
         Map<String, List<String>> planMap = new HashMap<>();
 
-        planMap.put("hipertensosBajoDePeso", Arrays.asList(
+        planMap.put("HipertensosBajoDePeso", Arrays.asList(
                 "Lunes - Aerobicos - Saltos De Cuerda - Moderada",
                 "Lunes - Aerobicos - Saltos De Tijera- Moderada",
                 "Lunes - Aerobicos - Caminata Rápida - Baja",
