aa2f928fb3e63417b84d94a7ab3021b18844f60a
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-16 14:48:49 -0500

    Update

--- DSB/src/main/java/com/DevSalud/DSB/Service/UserServices.java
+++ DSB/src/main/java/com/DevSalud/DSB/Service/UserServices.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.DevSalud.DSB.Exception.*;
-import com.DevSalud.DSB.Model.UserModel;
 import com.DevSalud.DSB.Repository.UserRepository;
 
 @Service
