c2a31cf241ee73e3328921e10aa205862aab9813
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-20 19:53:41 -0500

    Update Planes Eliminados

--- DSB/src/main/java/com/DevSalud/DSB/Controller/PlanAlimentController.java
+++ DSB/src/main/java/com/DevSalud/DSB/Controller/PlanAlimentController.java
@@ -2,14 +2,19 @@ package com.DevSalud.DSB.Controller;
 
 import com.DevSalud.DSB.Model.UserModel;
 import com.DevSalud.DSB.Service.UserServices;
+import com.google.gson.*;
 import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 @Data
 @Controller
@@ -19,21 +24,63 @@ public class PlanAlimentController {
     @Autowired
     private UserServices userService;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @GetMapping("/PlanAlimentos")
     public String listAlimetos(Model model, HttpSession session) {
-        Long userId = (Long) session.getAttribute("UsuarioId");
-        if (userId != null) {
-            UserModel user = userService.getUserById(userId);
-            if (user != null) {
-                return "/Health_Plans/Food/PlanesAlimenticios"; // Vista donde se mostrarán los planes
+        try {
+            Long userId = (Long) session.getAttribute("UsuarioId");
+            if (userId != null) {
+                UserModel user = userService.getUserById(userId);
+                if (user != null) {
+                    // Recargar el archivo JSON desde la carpeta resources
+                    Resource resource = resourceLoader.getResource("classpath:/static/Json/PlanesAlimenticios.json");
+                    String content = new String(Files.readAllBytes(resource.getFile().toPath()));
+                    // Parsear el JSON usando Gson
+                    Gson gson = new Gson();
+                    JsonObject jsonObject = gson.fromJson(content, JsonObject.class);
+                    // Extraer las opciones
+                    List<String> hipertensosBajoDePeso = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> hipertensosSobrePeso = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> hipertensosObesidadTipo1y2 = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> hipertensosObesidadTipo3y4 = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo1BajoDePeso = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo1SobrePeso = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo1ObesidadTipo1y2 = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo1ObesidadTipo3y4 = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo2BajoDePeso = extractJsonArray(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo2SobrePeso = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo2ObesidadTipo1y2 = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    List<String> diabetesTipo2ObesiadaTipo3y4 = extractAlimentsMap(jsonObject, "IntensidadEjercicio");
+                    model.addAttribute("FoodPlans", user);
+                } else {
+                    model.addAttribute("error", "Usuario no encontrado.");
+                    return "redirect:/Api/Users/Login";
+                }
             }
+        } catch (IOException | JsonSyntaxException e) {
+            e.printStackTrace();
+            model.addAttribute("jsonData", "Error leyendo el archivo JSON: " + e.getMessage());
         }
-        model.addAttribute("error", "Usuario no encontrado.");
-        return "redirect:/Api/Users/Login"; // Redirige a login si el usuario no se encuentra
+        return "/Health_Plans/Food/PlanesAlimenticios";
+    }
+
+    private Map<String, List<String>> extractAlimentsMap(JsonObject alimentosJson) {
+        Map<String, List<String>> AlimentsMap = new HashMap<>();
+        for (Map.Entry<String, JsonElement> entry : alimentosJson.entrySet()) {
+            List<String> alimentosList = new ArrayList<>();
+            JsonArray jsonArray = entry.getValue().getAsJsonArray();
+            for (JsonElement element : jsonArray) {
+                alimentosList.add(element.getAsString());
+            }
+            AlimentsMap.put(entry.getKey(), alimentosList);
+        }
+        return AlimentsMap;
     }
 
     @GetMapping("/TablaPlanesAlimenticios")
-    public String recomendarPlanesAlimenticios(){
+    public String recomendarPlanesAlimenticios() {
         return "PlanesAlimenticios";
     }
 
--- DSB/src/main/java/com/DevSalud/DSB/Controller/PlanExerciseController.java
+++ DSB/src/main/java/com/DevSalud/DSB/Controller/PlanExerciseController.java
@@ -6,7 +6,6 @@ import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
