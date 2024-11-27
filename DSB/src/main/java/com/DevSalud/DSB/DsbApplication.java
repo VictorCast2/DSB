1385b06610c61f5b3537b7654469fa16c6ae8958
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-11-27 15:58:57 -0500

    Update

--- DSB/src/main/java/com/DevSalud/DSB/DsbApplication.java
+++ DSB/src/main/java/com/DevSalud/DSB/DsbApplication.java
@@ -2,12 +2,30 @@ package com.DevSalud.DSB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DsbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsbApplication.class, args);
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
